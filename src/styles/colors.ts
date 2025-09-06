3b994d68d9019782cbd90ed9472de2efa0d4260f
oiviana <vianalucas80@gmail.com>
2025-09-06 19:09:57 -0300

    styles: more colors

--- src/styles/colors.ts
+++ src/styles/colors.ts
@@ -2,8 +2,10 @@ export const Colors = {
   primary: "#FFA800",
   secondary: "#6200EE",
   background: "#FFFFFF",
-  textPrimary: "#000000",
+  textBoldBlack: "#000000",
+  textMainGray: "#333333",
+  textSecondaryGray: "#939699ff",
   textSecondary: "#777777",
-  shadow: "#000000",
+  shadowBlack: "#000000",
   flagBackground: "#FF0000",
 };
