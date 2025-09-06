53160855ae8ecd48783761eddaaabc59bc477687
oiviana <vianalucas80@gmail.com>
2025-09-06 18:00:21 -0300

    test: vector icons in tab

--- src/routes/tabsNavigator.tsx
+++ src/routes/tabsNavigator.tsx
@@ -3,6 +3,7 @@ import HomeTest from "../views/HomeTest";
 import LoginTest from "../views/LoginTest";
 import SignupTest from "../views/SignupTest";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Icon from "react-native-vector-icons/MaterialIcons";
 
 const Tab = createBottomTabNavigator();
 
@@ -10,10 +11,30 @@ export default function TabsNavigatorApp() {
 
   return (
     <Tab.Navigator>
-      <Tab.Screen name="Início" component={HomeTest} />
-      <Tab.Screen name="Busca" component={LoginTest} />
-      <Tab.Screen name="Pedidos" component={SignupTest} />
-      <Tab.Screen name="Perfil" component={SignupTest} />
+      <Tab.Screen name="Início" component={HomeTest}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Icon name="home" size={30} color={"#FFA800"} />
+          ),
+        }} />
+      <Tab.Screen name="Busca" component={LoginTest}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Icon name="search" size={30} color={"#FFA800"} />
+          ),
+        }} />
+      <Tab.Screen name="Pedidos" component={SignupTest}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Icon name="shopping-basket" size={30} color={"#FFA800"} />
+          ),
+        }} />
+      <Tab.Screen name="Perfil" component={SignupTest}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Icon name="account-circle" size={30} color={"#FFA800"} />
+          ),
+        }} />
     </Tab.Navigator>
   )
 }
