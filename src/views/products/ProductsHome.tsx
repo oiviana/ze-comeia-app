efe4c3ce7039123dd71c834ae73615fa75c91e93
oiviana <vianalucas80@gmail.com>
2025-09-08 15:01:57 -0300

    feat: home structure

--- src/views/products/ProductsHome.tsx
+++ src/views/products/ProductsHome.tsx
@@ -1,11 +1,16 @@
 import { ScrollView } from "react-native";
 import { CategoryMainShelf } from "../../components/category/CategoryMainShelf";
+import ProductShelf from "../../components/product/ProductShelf";
+import { BrandsMainShelf } from "../../components/brands/BrandsMainShelf";
 
 export function ProductsHome(){
 
     return(
         <ScrollView>
             <CategoryMainShelf/>
+            <ProductShelf/>
+            <BrandsMainShelf/>
+            <ProductShelf/>
         </ScrollView>
     )
 }
