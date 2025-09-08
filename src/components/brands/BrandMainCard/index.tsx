a71d8f7132dd31b6dd31535fbb4697bb0075a948
oiviana <vianalucas80@gmail.com>
2025-09-08 15:01:44 -0300

    feat: brands shelf

---
+++ src/components/brands/BrandMainCard/index.tsx
@@ -0,0 +1,50 @@
+import { Image, ImageSourcePropType, StyleSheet, Text, View } from "react-native";
+import { Colors } from "../../../styles/colors";
+import { Shadow } from "react-native-shadow-2";
+
+interface  BrandMainCardProps {
+  
+    imageSource: ImageSourcePropType; // <-- nova prop para imagem
+}
+
+export function BrandMainCard({ imageSource }:  BrandMainCardProps) {
+    return (
+        <Shadow
+            startColor="#00000020"
+            distance={4} // aumentei um pouco para parecer sombra de verdade
+            offset={[0, 2]} 
+            corners={{
+                topStart: true,
+                topEnd: true,
+                bottomStart: true,
+                bottomEnd: true,
+            }}
+            containerStyle={{ marginBottom: 6, marginTop: 6 }}
+            style={{ borderRadius: 12 }}
+        >
+            <View style={styles.cardWrapper}>
+                <Image
+                    source={imageSource} // <-- usa a prop recebida
+                    style={styles.cardImage}
+                />
+            </View>
+        </Shadow>
+    )
+}
+
+const styles = StyleSheet.create({
+    cardWrapper: {
+        width: 110,
+        height: 70,
+        backgroundColor: Colors.backgroundWhite,
+        borderRadius: 12,
+        marginRight: 8,
+        overflow: "hidden",
+        alignItems:"center",
+        justifyContent:"center"
+    },
+    cardImage: {
+        width: 75,
+        height: 45,
+    },
+});
---
+++ src/components/brands/BrandsMainShelf/index.tsx
@@ -0,0 +1,52 @@
+import { FlatList, Text, View } from "react-native";
+import { BrandMainCard } from "../BrandMainCard";
+
+export function BrandsMainShelf() {
+    const categories = [
+        { id: "1", image: require("../../../assets/images/mock/brahma-logo.png") },
+        { id: "2", image: require("../../../assets/images/mock/heineken-logo.png") },
+        { id: "3", image: require("../../../assets/images/mock/Jack-Daniels-Logo.png") },
+        { id: "4", image: require("../../../assets/images/mock/brahma-logo.png") },
+        { id: "5", image: require("../../../assets/images/mock/heineken-logo.png") },
+        { id: "6", image: require("../../../assets/images/mock/Jack-Daniels-Logo.png") },
+        { id: "7", image: require("../../../assets/images/mock/brahma-logo.png") },
+        { id: "8", image: require("../../../assets/images/mock/heineken-logo.png") },
+        { id: "9", image: require("../../../assets/images/mock/Jack-Daniels-Logo.png") },
+        { id: "10", image: require("../../../assets/images/mock/brahma-logo.png") },
+        { id: "11", image: require("../../../assets/images/mock/heineken-logo.png") },
+        { id: "12", image: require("../../../assets/images/mock/Jack-Daniels-Logo.png") },
+    ];
+
+    // Função para agrupar de 2 em 2
+    const chunkArray = (array: any[], size: number) => {
+        const result = [];
+        for (let i = 0; i < array.length; i += size) {
+            result.push(array.slice(i, i + size));
+        }
+        return result;
+    };
+
+    const groupedCategories = chunkArray(categories, 2);
+
+    return (
+        <View>
+            <Text style={{ marginBottom: 8 }}>Navegue por marcas</Text>
+            <FlatList
+                data={groupedCategories}
+                keyExtractor={(_, index) => index.toString()}
+                horizontal
+                showsHorizontalScrollIndicator={false}
+                contentContainerStyle={{ paddingHorizontal: 9 }}
+                renderItem={({ item }) => (
+                    <View style={{ marginRight: 12 }}>
+                        {item.map((brand) => (
+                            <View key={brand.id} style={{ marginBottom: 12 }}>
+                                <BrandMainCard imageSource={brand.image} />
+                            </View>
+                        ))}
+                    </View>
+                )}
+            />
+        </View>
+    )
+}
