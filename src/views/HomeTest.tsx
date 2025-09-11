f45f7764e74adac876b97c06dbf24b43dd8e9ab0
oiviana <vianalucas80@gmail.com>
2025-09-11 01:02:22 -0300

    fix: home safe area view

--- src/views/HomeTest.tsx
+++ src/views/HomeTest.tsx
@@ -1,15 +1,15 @@
-import {ScrollView, Text} from 'react-native';
+import { ScrollView, Text } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 import ProductShelf from '../components/product/ProductShelf';
 
 export default function MainHome() {
-    return(
-        <ScrollView>
-            <Text>
-                Home Test
-            </Text>
-            <ProductShelf shelfTitle='Teste'/>
-            <ProductShelf shelfTitle='Teste 2'/>
-            <ProductShelf shelfTitle='Teste 4'/>
-        </ScrollView>
+    return (
+        <SafeAreaView style={{ flex: 1 }}>
+            <ScrollView>
+                <ProductShelf shelfTitle='Teste' />
+                <ProductShelf shelfTitle='Teste 2' />
+                <ProductShelf shelfTitle='Teste 4' />
+            </ScrollView>
+        </SafeAreaView>
     )
 }
